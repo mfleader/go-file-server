7bc5cf64f3f1e43471cfffaf561e4b44cd35e6e2
mleader <mleader@redhat.com>
2021-05-27 21:23:40 -0400

    init readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-file-server
+
+golang file server
--- cm/pod-compose.sh
+++ cm/pod-compose.sh
@@ -2,15 +2,15 @@
 
 podman rm -f go-api go-index
 podman pod rm gofiles
-podman pod create gofiles --publish 3000:80
+podman pod create --name=gofiles --publish 3000:80
 
-# podman run --detach \
-#   --pod gofile \
-#   -v "$PWD/nginx.conf:/etc/nginx/nginx.conf:z" \
-#   -v "$PWD/results:/usr/share/nginx/html/results:z" \ 
-#   -p 8989:80 \
-#   --name nginx
-#   nginx
+podman run --detach \
+  --pod gofiles \
+  -v "$PWD/nginx/nginx.conf:/etc/nginx/nginx.conf:z" \
+  -v "$PWD/results:/usr/share/nginx/html/results:z" \
+  --name go-index \
+  nginx:1.16
+
 
 # podman run --detach \
 #   --pod gofile \
@@ -19,3 +19,10 @@ podman pod create gofiles --publish 3000:80
 #   -p 8989:80 \
 #   --name go-index
 #   go-index
+
+
+podman run --detach \
+  --pod gofiles \
+  --name go-api \
+  -v "$PWD/results:/app/results:z" \
+  localhost/go-api
